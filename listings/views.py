f91ac49deb3fdbf12562dd9cb889e0487ce45e8b
Jue Ning <juening@gmail.com>
2021-03-08 23:05:27 -0500

    filter search form

--- listings/views.py
+++ listings/views.py
@@ -31,9 +31,42 @@ def listing(request, listing_id):
 
 def search(request):
 
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__exact=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if (int(price) >= 1000000):
+            filterValue = queryset_list.filter(price__gte=price)
+            queryset_list = filterValue
+        else:
+            filterValue = queryset_list.filter(price__lte=price)
+            queryset_list = filterValue
+
     context = {
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
-        'state_choices': state_choices
+        'state_choices': state_choices,
+        'listings': queryset_list
     }
     return render(request, 'listings/search.html', context)
