6608ec174c03ca72150f3e99ba04b14739680089
Jue Ning <juening@gmail.com>
2021-03-10 23:40:55 -0500

    added login and register templates

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, redirect
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        print('Registration successful')
+        return redirect('index')
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        print('Successful logged in')
+        return redirect('index')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def dashboard(request):
